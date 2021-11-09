d9940460e752e3887015a8c3f02bc3c611b935b7
SunYong <924466726@qq.com>
2021-11-09 12:37:26 +0800

    Update Ellipse_Fill.cpp

--- Algorithms/Ellipse_Fill.cpp
+++ Algorithms/Ellipse_Fill.cpp
@@ -4,11 +4,11 @@ void ellipse_fill(int x,int y,int rx,int ry, PixelSet& myset)
 {
     for(int i=x;i<=x+rx;++i){
         for(int j=y;j<=y+ry;++j){
-            if(((i-x)*(i-x)/rx/rx+(j-y)*(j-y)/ry) <= (rx*rx+ry*ry)){
+            if(((i-x)*(i-x)/rx/rx+(j-y)*(j-y)/ry/ry) <=1 ){
                 myset.add(i,j);
-                myset.add(2*rx-i,j);
-                myset.add(i,2*ry-j);
-                myset.add(2*rx-i,2*ry-j);
+                myset.add(2*x-i,j);
+                myset.add(i,2*y-j);
+                myset.add(2*x-i,2*y-j);
             }
             else
                 break;
